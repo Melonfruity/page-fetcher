03991a4baa13b2cddfb3e8a81c0dc195d08619fe
melonfruity <eric.luo24@gmail.com>
2019-12-04 16:06:12 -0500

    done all edge cases specified

--- fetcher.js
+++ fetcher.js
@@ -9,16 +9,21 @@ const rl = readline.createInterface({
 });
 
 // Uses readline question to get input
-
-fs.exists('/etc/passwd', (exists) => {
+if(destination.substring(destination.length - 5, destination.length) !== '.html'){
+  console.log('Bad destination');
+  process.exit()
+}
+fs.exists(destination, (exists) => {
   console.log(exists ? 'it\'s there' : 'no passwd!');
   if(exists){
     rl.question(`Overwrite? Y/N\n`, (ans) => {
       if (ans.toUpperCase() === 'Y') {
         writeToFile(link, destination);
-      } 
+      }
       rl.close();
     })
+  } else {
+    writeToFile(link, destination);
   }
 });
 
@@ -26,6 +31,8 @@ const writeToFile = () => {
   request(`${link}`, (error, response, body) => {
     if(body){
       writeToHtmlFile(destination, body);
+    } else {
+      console.log('Bad Request');
     }
   });
 
@@ -35,6 +42,7 @@ const writeToFile = () => {
       const stats = fs.statSync(destination);
       const fileSizeInBytes = stats["size"];
       console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${destination}`);
+      process.exit();
     });
   }
 }
